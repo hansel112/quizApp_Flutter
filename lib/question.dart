58950baf840a1dc917f0ea172de734b55a3e06c6
HANSEL CARZERLET <124737107+hansel112@users.noreply.github.com>
2025-07-13 01:11:00 +0300

    Update question.dart

--- lib/question.dart
+++ lib/question.dart
@@ -1,9 +1,6 @@
 class Question {
-  String questionText = '';
-  bool questionAnswer = true;
+  String questionText;
+  bool questionAnswer;
 
-  Question({String q = '', bool a = true}) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
